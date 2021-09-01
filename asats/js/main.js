a283b4b737a7fde3c832ead247b4d339f32d6535
shahjalal <shahjalalkhan215@gmail.com>
2021-09-01 20:52:24 +0600

    Slider Path

--- asats/js/main.js
+++ asats/js/main.js
@@ -1,7 +1,43 @@
-const textArea = document.querySelector('.textArea')
-const count = document.querySelector('.count')
+const nextBtn = document.querySelector('.next-btn')
+const prevBtn = document.querySelector('.prev-btn')
+const slides = document.querySelectorAll('.slide')
+const slidesIcon = document.querySelectorAll('.icon')
 
-function counter(){   
-   const textLenth = textArea.value.length;
-   count.innerHTML = `${textLenth}`
+const numberOfSlides = slides.length;
+var slideNumber = 0;
+
+// Image Slider Next Button
+nextBtn.addEventListener('click', slideFunc)
+function slideFunc () {
+   slides.forEach((slide) => {
+      slide.classList.remove('active')
+   })
+   slidesIcon.forEach((icons) => {
+      icons.classList.remove('active')
+   })
+   slideNumber++
+   if(slideNumber > numberOfSlides -1){
+      slideNumber = 0;
+   }
+   slides[slideNumber].classList.add('active')
+   slidesIcon[slideNumber].classList.add('active')
 }
+
+// Images Slider Prev Button
+prevBtn.addEventListener('click', () => {
+   slideNumber--
+   slides.forEach((slide) => {
+      slide.classList.remove('active')
+   })
+   slidesIcon.forEach((icons) => {
+      icons.classList.remove('active')
+   })
+   if(slideNumber < 0){
+      slideNumber = numberOfSlides - 1
+   }      
+   slides[slideNumber].classList.add('active')
+   slidesIcon[slideNumber].classList.add('active')
+})
+
+// Slider Infinite
+setInterval(slideFunc, 5000)
